d3f2df8598cd1c4b368d98cae42f02aead469ef0
Soham-Darak <darakss@rknec.edu>
2026-01-12 21:05:18 +0530

    Making Sign up page

--- src/app/(auth)/sign-up/page.js
+++ src/app/(auth)/sign-up/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SignInPage from '../../components/Authentication/SignUpCard'
 
-const page = () => {
+const SignUpPage = () => {
   return (
-    <div>page</div>
+    <div>
+      <SignInPage></SignInPage>
+    </div>
   )
 }
 
-export default page
+export default SignUpPage
