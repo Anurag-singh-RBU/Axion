70775bdb61f88e70c94b412ef84bacc76d29dae7
BhumikaDudhe <dudheby@rknec.edu>
2026-01-15 00:27:58 +0530

    Button added in SignUpCard.js and LoginCard.js

--- src/app/components/Authentication/LoginCard.js
+++ src/app/components/Authentication/LoginCard.js
@@ -1,4 +1,10 @@
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/login-form";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+export function ButtonDefault() {
+  return <Button>Button</Button>
+}
+
 
 export default function LoginPage() {
   return (
@@ -6,6 +12,17 @@ export default function LoginPage() {
       <div className="pointer-events-none absolute inset-0 z-10">
       </div>
       <div className="flex w-full max-w-sm flex-col gap-6 z-10">
+
+        <a href="/">
+      <Button className="flex w-22 items-center gap-3 px-5 py-2 rounded-full bg-white shadow-sm hover:bg-white hover:text-gray-800">
+      <span className="flex items-center justify-center w-4 h-4 bg-gray-400 rounded-full">
+  <ChevronLeft className="w-10 h-10 text-white" />
+</span>
+  <span className="text-gray-800 font-FT font-semibold uppercase tracking-wide">
+    BACK
+  </span>
+</Button>
+</a>
         <LoginForm/>
       </div>
     </div>
--- src/app/components/Authentication/SignUpCard.js
+++ src/app/components/Authentication/SignUpCard.js
@@ -1,4 +1,11 @@
-import { SignUpForm } from "../../../components/signup-form"
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { SignUpForm } from "../../../components/signup-form";
+
+export function ButtonDefault() {
+  return <Button>  Button</Button>
+}
+
 
 export default function SignUpCard() {
   return (
@@ -6,6 +13,16 @@ export default function SignUpCard() {
       <div className="pointer-events-none absolute inset-0 z-10">
       </div>
       <div className="flex w-full max-w-sm flex-col gap-6 z-10">
+        <a href="/sign-in">
+      <Button className="flex w-22 items-center gap-3 px-5 py-2 rounded-full bg-white shadow-sm hover:bg-white hover:text-gray-800">
+      <span className="flex items-center justify-center w-4 h-4 bg-gray-400 rounded-full">
+  <ChevronLeft className="w-10 h-10 text-white" />
+</span>
+  <span className="text-gray-800 font-FT font-semibold uppercase tracking-wide">
+    BACK
+  </span>
+</Button>
+</a>
         <SignUpForm/>
       </div>
     </div>
