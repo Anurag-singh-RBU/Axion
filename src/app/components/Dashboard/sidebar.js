58e8bbcf22ef0dc7af2c8d7df5c498b66f14a9cb
ANURAG SINGH <anuragsingh260805@gmail.com>
2026-01-24 00:41:51 +0530

    added sidebar

--- src/app/components/Dashboard/sidebar.js
+++ src/app/components/Dashboard/sidebar.js
@@ -5,29 +5,10 @@ import Image from 'next/image'
 export const Sidebar = () => {
   return (
     <aside className='h-full bg-neutral-50 p-4 w-full border-r border-gray-100'>
-        <div className="flex items-center gap-3 mb-4 bg-gradient-to-r from-blue-200 via-indigo-100 to-violet-100 p-[6px] rounded-lg">
+        <div className="flex items-center gap-3 mb-4 bg-linear-to-r from-blue-200 via-indigo-100 to-violet-100 p-[6px] rounded-lg">
             <div className="flex items-center bg-white/90 p-2 rounded-lg">
-                <Image 
-                    src="/logo.png" 
-                    alt="Axion Logo" 
-                    width={36} 
-                    height={36} 
-                    className="object-cover"
-                />
-                <Link 
-                    href="/"
-                    className={`
-                        ml-3 
-                        bg-gradient-to-r 
-                        from-blue-500 via-purple-400 to-indigo-400 
-                        bg-clip-text 
-                        text-transparent 
-                        font-bold 
-                        font-JBM 
-                        text-xl 
-                        tracking-widest
-                    `}
-                >
+                <Image src="/logo.png" alt="Axion Logo" width={36} height={36} className="object-cover"/>
+                <Link href="/" className="ml-3 bg-linear-to-r from-blue-500 via-purple-400 to-indigo-400 bg-clip-text text-transparent font-bold font-JBM text-xl tracking-widest">
                     AXION
                 </Link>
             </div>
