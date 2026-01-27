6b1429530efbd3f9d788610a42455e9c1c55c600
ANURAG SINGH <anuragsingh260805@gmail.com>
2026-01-28 01:12:20 +0530

    added workspace form

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,12 +3,14 @@
 import { Sidebar } from "@/app/components/Dashboard/sidebar";
 import Header from "@/app/components/Dashboard/header";
 import { SidebarProvider, useSidebar } from "@/app/components/Dashboard/sidebar-context";
+import ClickSpark from "../../components/ClickSpark";
 
 function DashboardContent({ children }) {
     const { isCollapsed, isMobile } = useSidebar();
     
     return (
         <div className="min-h-screen">
+            <ClickSpark sparkColor='rgb(59,130,246)' sparkSize={10} sparkRadius={15} sparkCount={8} duration={400}>
             <div className="flex w-full h-full">
                 {/* Desktop Sidebar - Fixed */}
                 <div className={`fixed left-0 top-0 hidden lg:block h-full overflow-y-auto transition-all duration-300 z-40 ${
@@ -34,6 +36,7 @@ function DashboardContent({ children }) {
                     </div>
                 </div>
             </div>
+        </ClickSpark>
         </div>
     );
 }
